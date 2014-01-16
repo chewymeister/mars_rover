3839edeed2f23694613b773911c4e73032e8a8d7
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-16 21:41:52 +0000

    rover can move forward

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,31 +1,49 @@
 class Rover
   DIRECTIONS = ['N','E','S','W']
+  attr_reader :facing
 
   def move(location, command)
     coords = location.split(' ')
     @x = coords[0].to_i
     @y = coords[1].to_i
-    @facing = coords[2]
-    @increment = determine_from(command)
-    @facing = DIRECTIONS[calculate(DIRECTIONS.index(@facing) ,@increment)]
+    @facing = DIRECTIONS.index(coords[2])
+    determine_from(command)
   end
 
   def calculate(start, length)
     start += length 
   end
 
+  def current_direction
+    DIRECTIONS[@facing]
+  end
+
   def determine_from(command)
     commands = command.split
-    number = commands.inject(0) do |sum, direction|
+    commands.each do |direction|
       if direction == 'L'
-        sum -= 1
+        @facing -= 1
       elsif direction == 'R'
-        sum += 1
+        @facing += 1
+      else
+        move_a_space
       end
     end
   end
 
+  def move_a_space
+    if current_direction == 'N'
+      @y += 1
+    elsif current_direction == 'E'
+      @x += 1
+    elsif current_direction == 'S'
+      @y -= 1
+    elsif current_direction == 'W'
+      @x -= 1
+    end
+  end
+
   def current_location
-    "#{@x.to_s} #{@y.to_s} #{@facing}"
+    "#{@x.to_s} #{@y.to_s} #{current_direction}"
   end
 end
--- spec/lib/rover_spec.rb
+++ spec/lib/rover_spec.rb
@@ -2,17 +2,22 @@ require 'rspec'
 require './lib/rover'
 
 describe Rover do
+  let(:test_rover) { Rover.new }
   it 'can move left' do
-    test_rover = Rover.new
     test_rover.move('0 0 N', 'L')
 
     expect(test_rover.current_location).to eq '0 0 W'
   end
 
   it 'can move right' do
-    test_rover = Rover.new
     test_rover.move('0 0 N', 'R')
 
     expect(test_rover.current_location).to eq '0 0 E'
+  end
+
+  it 'can move forward' do
+    test_rover.move('0 0 N', 'F')
+
+    expect(test_rover.current_location).to eq '0 1 N'
   end
 end
