91f68230a4a7acf2677f2737c8bf8a571112ac5c
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-17 10:25:36 +0000

    Refactored rover.rb

--- lib/rover.rb
+++ lib/rover.rb
@@ -1,49 +1,82 @@
 class Rover
   DIRECTIONS = ['N','E','S','W']
-  attr_reader :facing
+  FACING = { 'L' => -1, 'R' => 1 }
+  ONE_STEP_IN = { 'N' => 1, 'S' => -1, 'E' => 1, 'W' => -1 }
 
-  def move(location, command)
-    coords = location.split(' ')
-    @x = coords[0].to_i
-    @y = coords[1].to_i
-    @facing = DIRECTIONS.index(coords[2])
-    determine_from(command)
+  def move(coords, command)
+    assign(coords) 
+    determine_move_from(command)
   end
 
-  def calculate(start, length)
-    start += length 
+  def current_location
+    "#{@x.to_s} #{@y.to_s} #{current_direction}"
+  end
+
+  private
+
+  def assign(coords)
+    @x, @y, @cardinal_index = x(coords), y(coords), cardinal(coords) 
+  end
+
+  def cardinal(coords)
+    DIRECTIONS.index(list(coords)[2])
+  end
+
+  def y(coords)
+    list(coords)[1].to_i
+  end
+
+  def x(coords)
+    list(coords)[0].to_i
+  end
+  
+  def list(coords)
+    coords.split(' ')
   end
 
   def current_direction
-    DIRECTIONS[@facing]
+    DIRECTIONS[@cardinal_index]
   end
 
-  def determine_from(command)
-    commands = command.chars
-    commands.each do |direction|
-      if direction == 'L'
-        @facing -= 1
-      elsif direction == 'R'
-        @facing += 1
+  def determine_move_from(commands)
+    list_of(commands).each do |direction|
+      if turn_left_or_right?(direction)
+        turn_to_face(direction)
       else
         move_a_space
       end
     end
   end
 
+  def turn_left_or_right?(direction)
+    direction == 'L' || direction == 'R'
+  end
+
+  def turn_to_face(direction)
+    @cardinal_index += FACING[direction]
+  end
+
+  def list_of(command)
+    command.chars
+  end
+
   def move_a_space
-    if current_direction == 'N'
-      @y += 1
-    elsif current_direction == 'E'
-      @x += 1
-    elsif current_direction == 'S'
-      @y -= 1
-    elsif current_direction == 'W'
-      @x -= 1
+    if facing_north_or_south?
+      go_vertical
+    else
+      go_horizontal
     end
   end
 
-  def current_location
-    "#{@x.to_s} #{@y.to_s} #{current_direction}"
+  def go_horizontal
+    @x += ONE_STEP_IN[current_direction]
+  end
+
+  def go_vertical
+    @y += ONE_STEP_IN[current_direction]
+  end
+
+  def facing_north_or_south?
+    current_direction == 'N' || current_direction == 'S'
   end
 end
