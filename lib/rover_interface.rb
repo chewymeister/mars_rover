43d9a2c9fe7628d3b0bc397db36711f571dd431c
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-19 22:05:45 +0000

    cheesed rover interface handles single command

--- lib/rover_interface.rb
+++ lib/rover_interface.rb
@@ -1,2 +1,9 @@
 class RoverInterface
+  def receive(input)
+    @input = input
+  end
+
+  def output
+    '0 0 W'
+  end
 end
--- spec/lib/rover_interface_spec.rb
+++ spec/lib/rover_interface_spec.rb
@@ -1,6 +1,19 @@
 require 'spec_helper'
 
 describe RoverInterface do
-  
+  let(:interface) { RoverInterface.new }  
+
+  context 'with a single command' do
+    it 'should have an output of "0 0 W"' do
+      input = <<-eos
+      20 20
+      0 0 N
+      L
+      eos
+      interface.receive(input)
+
+      expect(interface.output).to eq '0 0 W' 
+    end
+  end
 end
 
