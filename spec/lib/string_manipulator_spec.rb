4f94a5c129a21c6bec4194fb276e5b4fac50d96e
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-19 20:10:13 +0000

    tidied up contexts in string_manipulator_spec

--- spec/lib/string_manipulator_spec.rb
+++ spec/lib/string_manipulator_spec.rb
@@ -2,30 +2,25 @@ require 'spec_helper'
 
 describe StringManipulator do
   context 'extracts the boundary' do
-    context 'should identify upper x boundary' do
-      it 'as 10' do
-        manipulator = StringManipulator.new('10 10')
+    context 'of value 10 10' do
+      let(:manipulator) { StringManipulator.new('10 10') }
 
+      it 'upper_x is 10' do
         expect(manipulator.output[:boundaries][:upper_x]).to eq 10
       end
 
-      it 'as 2000' do
-        manipulator = StringManipulator.new('1000 2000')
-
-        expect(manipulator.output[:boundaries][:upper_x]).to eq 1000
+      it 'upper_y is 10' do
+        expect(manipulator.output[:boundaries][:upper_y]).to eq 10
       end
     end
 
-    context 'should identify upper y boundary' do
-      it 'as 10' do
-        manipulator = StringManipulator.new('10 10')
-
-        expect(manipulator.output[:boundaries][:upper_y]).to eq 10
+    context 'of value 2000 2000' do
+      let(:manipulator) { StringManipulator.new('2000 2000') }
+      it 'upper_x is 2000' do
+        expect(manipulator.output[:boundaries][:upper_x]).to eq 2000
       end
 
-      it 'as 2000' do
-        manipulator = StringManipulator.new('2000 2000')
-
+      it 'upper_y is 2000' do
         expect(manipulator.output[:boundaries][:upper_y]).to eq 2000
       end
     end
@@ -41,22 +36,17 @@ describe StringManipulator do
             M
           eos
         end
+        let(:manipulator) { StringManipulator.new(string) }
 
         it 'with the x coord being 0' do
-          manipulator = StringManipulator.new(string)
-
           expect(manipulator.output[:instructions][0][:coords][:x]).to eq 0
         end
 
         it 'with the y coord being 0' do
-          manipulator = StringManipulator.new(string)
-
           expect(manipulator.output[:instructions][0][:coords][:y]).to eq 1
         end
 
         it 'with the cardinal coord being "N"' do
-          manipulator = StringManipulator.new(string)
-
           expect(manipulator.output[:instructions][0][:coords][:cardinal]).to eq 'N'
         end
       end
@@ -69,22 +59,17 @@ describe StringManipulator do
             M
           eos
         end
+        let(:manipulator) { StringManipulator.new(string) }
 
         it 'with the x coord being 0' do
-          manipulator = StringManipulator.new(string)
-
           expect(manipulator.output[:instructions][0][:coords][:x]).to eq 5
         end
 
         it 'with the y coord being 0' do
-          manipulator = StringManipulator.new(string)
-
           expect(manipulator.output[:instructions][0][:coords][:y]).to eq 6
         end
 
         it 'with the cardinal coord being "S"' do
-          manipulator = StringManipulator.new(string)
-
           expect(manipulator.output[:instructions][0][:coords][:cardinal]).to eq 'S'
         end
       end
@@ -115,7 +100,7 @@ describe StringManipulator do
         end
       end
 
-      context 'without a string that contains boundaries' do
+      context 'with a string that does not contain boundaries' do
         it 'as MMM' do
           string = <<-eos
             5 6 S
@@ -138,24 +123,27 @@ describe StringManipulator do
       end
     end
   end
+
   context 'handles multiple commands' do
     let(:string) do
       <<-eos
+        10 10
         5 6 S
         M
         7 7 N
         LMRMLLM
       eos
     end
+    let(:manipulator) { StringManipulator.new(string) }
+    let(:first_result) { { coords: { x: 5, y: 6, cardinal: 'S' }, commands: 'M' } }
+    let(:second_result) { { coords: { x: 7, y: 7, cardinal: 'N' }, commands: 'LMRMLLM' } }
 
-    it 'will have two sets of instructions' do
-      manipulator = StringManipulator.new(string)
-
-      first_result = { coords: { x: 5, y: 6, cardinal: 'S' }, commands: 'M' }
-      second_result = { coords: { x: 7, y: 7, cardinal: 'N' }, commands: 'LMRMLLM' }
+    it 'first set of instructions should equal first result' do
       expect(manipulator.output[:instructions].first).to eq first_result
+    end
+
+    it 'second set of instructions should equal second result' do
       expect(manipulator.output[:instructions].last).to eq second_result
-      expect(manipulator.output[:boundaries].empty?).to be_true
     end
   end
 end
