11c11c784bf0059a5176284f9bc5131953b4f381
Jonathan Tsang <chewymeister88@googlemail.com>
2014-01-20 10:48:35 +0000

    rover interface can handle multiple commands, project now fulfils requirements

--- lib/rover_interface.rb
+++ lib/rover_interface.rb
@@ -1,19 +1,40 @@
 class RoverInterface
   def receive(input)
-    @input = input
     @processor = StringManipulator.new(input)
     @data = @processor.output
-    @boundaries = @data[:boundaries]
   end
 
   def output
-    @rovers = @data[:instructions].inject([]) do |collection, instruction|
-      processed_input = { boundaries: @boundaries, instructions: instruction }
-      collection << Rover.new(processed_input)
-    end
+    extract_rover_locations_into_string.rstrip
+  end
 
-    @rovers.inject('') do |string, rover|
-     string += "#{rover.current_location}\n"
-    end.rstrip
+  def extract_rover_locations_into_string
+    initiate_rovers.inject('') { |output, rover| output += location_of(rover) }
+  end
+
+  def location_of(rover)
+    "#{rover.current_location}\n"
+  end
+
+  def initiate_rovers
+    list_of_instructions.inject([]) do |rovers, instructions|
+      rovers << new_rover_with(instructions)
+    end
+  end
+
+  def new_rover_with(instructions)
+    Rover.new(processed_input_from(instructions))
+  end
+
+  def list_of_instructions
+    @data[:instructions]
+  end
+
+  def boundaries
+    @data[:boundaries]
+  end
+
+  def processed_input_from(instructions)
+    { boundaries: boundaries, instructions: instructions }
   end
 end
--- spec/lib/rover_interface_spec.rb
+++ spec/lib/rover_interface_spec.rb
@@ -26,5 +26,57 @@ describe RoverInterface do
       expect(interface.output).to eq '0 0 E' 
     end
   end
-end
 
+  context 'with multiple commands' do
+    it 'should have an output of "3 3 E"' do
+      input = <<-eos
+        20 20
+        0 0 N
+        LMMRMMMRMMM
+      eos
+      interface.receive(input)
+
+      expect(interface.output).to eq '3 3 E' 
+    end
+
+    it 'should have an output of "1 2 W"' do
+      input = <<-eos
+        20 20
+        0 0 N
+        RMMMLMLMMRML
+      eos
+      interface.receive(input)
+
+      expect(interface.output).to eq '1 2 W' 
+    end
+
+    context 'with a large boundary' do
+      it 'should have an output of "47 35 S"' do
+        input = <<-eos
+          100 100
+          25 32 E
+          MMMMMMMMMMMMMMMMMLMMMMMMMMRMMMMMRMMMMM
+        eos
+        interface.receive(input)
+
+        expect(interface.output).to eq '47 35 S' 
+      end
+    end
+  end
+
+  context 'with multiple instructions' do
+    it 'should have an output of "1 2 W", and "8 10 W"' do
+      input = <<-eos
+        20 20
+        0 0 N
+        RMMMLMLMMRML
+        10 10 S
+        MMRMRMMLM
+      eos
+        
+      interface.receive(input)
+
+      expect(interface.output).to eq "1 2 W\n8 10 W" 
+    end
+  end
+end
